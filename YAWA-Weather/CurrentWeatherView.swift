e23ac82f58a19bac41d50d3a5edd4c10404e5554
Edward P. Kelly <ekelly@EdK-MBP.local>
2016-04-04 13:28:46 -0400

    After many, many auto layout adjustments, committing an almost solid portrait layout with functioning constraints.

---
+++ YAWA-Weather/CurrentWeatherView.swift
@@ -0,0 +1,28 @@
+//
+//  CurrentWeatherView.swift
+//  YAWA-Weather
+//
+//  Created by Edward P. Kelly on 4/1/16.
+//  Copyright © 2016 Edward P. Kelly, LLC. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeatherView: UIView {
+    
+    
+    
+    override func awakeFromNib()
+    {
+        print("I am a current forecast view!")
+    }
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
