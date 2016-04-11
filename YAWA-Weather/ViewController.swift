c1dca14729bfa1b944df0708457efe7d9c13de79
Edward P. Kelly <ekelly@EdK-MBP.local>
2016-04-11 12:11:05 -0400

    Buttoning up UI and adding details view class.

--- YAWA-Weather/ViewController.swift
+++ YAWA-Weather/ViewController.swift
@@ -10,21 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private enum WeatherViewState: Int
+    {
+        case Current = 0, Details
+    }
+    
     @IBOutlet weak var searchBar:UISearchBar!
     @IBOutlet weak var segmentBar:UISegmentedControl!
     @IBOutlet weak var currentWeatherView:CurrentWeatherView!
     @IBOutlet weak var scrollView:UIScrollView!
+    @IBOutlet weak var detailsView:UIView!
     
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        print("LOAD!")
+        segmentBar.addTarget(self, action: #selector(ViewController.toggleView(_:)), forControlEvents: .ValueChanged)
+        
+        detailsView.hidden = true
     }
     
     override func viewDidLayoutSubviews()
     {
         scrollView.contentOffset.y = searchBar.bounds.size.height
+    }
+    
+    func toggleView(segmentControl: UISegmentedControl)
+    {
+        let index = segmentControl.selectedSegmentIndex
+        if let viewToShow = WeatherViewState(rawValue: index) {
+            switch viewToShow {
+            case .Current:
+                currentWeatherView.hidden = false
+                detailsView.hidden = true
+            case .Details:
+                currentWeatherView.hidden = true
+                detailsView.hidden = false
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
